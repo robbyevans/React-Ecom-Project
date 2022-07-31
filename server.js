d01c7e8990a8aed351a23daea60ffc1f476a7dad
Evans Rutto <robbyevans001@gmail.com>
2022-07-31 15:45:13 +0300

    done

--- server.js
+++ server.js
@@ -5,15 +5,14 @@ const middlewares=jsonServer.defaults({
   static:"./build",
 });
 
-const port=process.env.Port || 5000;
+const PORT=process.env.PORT || 8000;
 server.use(middlewares);
-server.use(
-  jsonServer.rewriter({
+server.use(jsonServer.rewriter({
     "/api/*":"/$1"
   })
 );
 
 server.use(router);
-server.listen(port),()=>{
-  console.log(`server is running 0n ${port}`)
+server.listen(PORT),()=>{
+  console.log(`Server is running 0n ${PORT}`)
 }
